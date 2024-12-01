3a195de1cc91de97d6b0ffcefc53b621819e2adc
cadyphan <cadyphan790@gmail.com>
2024-12-01 16:51:44 -0500

    purchaselist

--- app/src/main/java/edu/uga/cs/roommateshopping/PurchaseList.java
+++ app/src/main/java/edu/uga/cs/roommateshopping/PurchaseList.java
@@ -5,15 +5,20 @@ import java.util.List;
 public class PurchaseList {
     private List<ShoppingItem> purchaseList;
     private String purchasedBy;
-
+    private String name;
+    private String key;
     public PurchaseList() {
         this.purchaseList = null;
-        purchasedBy = null;
+        this.purchasedBy = null;
+        this.name = null;
+        this.key = null;
     }
 
-    public PurchaseList(List<ShoppingItem> shoppingList, String purchasedBy) {
+    public PurchaseList(List<ShoppingItem> shoppingList, String purchasedBy, String name) {
         this.purchaseList = shoppingList;
         this.purchasedBy = purchasedBy;
+        this.name = name;
+        this.key = null;
     }
 
     public void setPurchaseList(List<ShoppingItem> shoppingList) {
@@ -25,6 +30,16 @@ public class PurchaseList {
     public void setPurchasedBy(String purchasedBy) {
         this.purchasedBy = purchasedBy;
     }
+
     public String getPurchasedBy () {return purchasedBy;}
+
+    public String getListName () {return name;}
+
+    public void setListName (String name) { this.name = name;}
+
+    public String getKey () {return key;}
+
+    public void setKey (String key) { this.key = key;}
+
 
 }
